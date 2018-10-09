3cf919d9d6a3f13f6b8137c672933feab34d80d4
clement <clement.vercucque@orange.fr>
2018-10-09 18:28:56 +0200

    add basic auth

--- index.js
+++ index.js
@@ -8,7 +8,27 @@ const cookieParser = require('cookie-parser');
 
 // SETUP APP
 const app = express();
-
+var basicAuth = require('basic-auth');
+ 
+var auth = function (req, res, next) {
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    res.sendStatus(401);
+    return;
+  }
+  if (user.name === 'amy' && user.pass === 'passwd123') {
+    next();
+  } else {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    res.sendStatus(401);
+    return;
+  }
+}
+ 
+app.get("/images", auth, function (req, res) {
+    res.send("This page is authenticated!")
+});
 app.use('/', express.static(__dirname + '/'));
 
 const maxFileSize = 4194304 // Taille du fichier en octets
